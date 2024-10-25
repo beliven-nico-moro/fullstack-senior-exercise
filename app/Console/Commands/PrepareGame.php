495c24fe55d49373b2e71d93feb812bf46783a92
Nico Moro <nico.moro@beliven.com>
2024-10-25 14:44:46 +0200

    fix: changed command

---
+++ app/Console/Commands/PrepareGame.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class StartGame extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'game:prepare';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Prepare the database for the game';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Preparing DB...');
+        $this->call('migrate:fresh');
+
+        $this->info('Seed DB...');
+        $this->call('db:seed');
+
+        $this->info('Game prepared!');
+    }
+}
